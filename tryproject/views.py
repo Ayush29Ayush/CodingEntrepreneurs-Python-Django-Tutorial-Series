7bff0238f2d609edf091fdf9b183b46613e7a12f
Ayush Senapati <ayush.senapati@cozentus.com>
2023-09-11 16:34:17 +0530

    trial commit

--- tryproject/views.py
+++ tryproject/views.py
@@ -3,14 +3,18 @@ To render HTML pages
 """
 from django.http import HttpResponse
 import random
+from articles.models import Article
 
 def home_view(request):
     name = "Ayush Senapati"
     number = random.randint(1,100)
-    HTML_STRING1 = f"""
-    <h1>Hello {name} - {number}</h1>
-    """
-    HTML_STRING2 = f"""
-    <h1>Hello {name} - {number}</h1>
-"""
+    # HTML_STRING1 = f"""
+    # <h1>Hello {name} - {number}</h1>
+    # """
+    # HTML_STRING2 = f"""
+    # <h1>Hello {name} - {number}</h1>
+    # """
+    article_obj = Article.objects.get(id=2)
+    article_title = article_obj.title
+    article_content = article_obj.content
     return HttpResponse(HTML_STRING1 + HTML_STRING2)
