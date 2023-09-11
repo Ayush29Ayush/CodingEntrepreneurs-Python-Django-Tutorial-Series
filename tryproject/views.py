54ed76647903c154eeccd68c90e0c5f1167f9f81
Ayush Senapati <ayush.senapati@cozentus.com>
2023-09-11 16:37:41 +0530

    jhvj

--- tryproject/views.py
+++ tryproject/views.py
@@ -16,5 +16,6 @@ def home_view(request):
     # """
     article_obj = Article.objects.get(id=2)
     article_title = article_obj.title
+    
     article_content = article_obj.content
     return HttpResponse(HTML_STRING1 + HTML_STRING2)
