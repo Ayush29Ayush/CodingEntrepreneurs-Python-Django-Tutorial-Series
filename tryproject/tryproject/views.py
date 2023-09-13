50c90cd219156d243e36f3a8f75540297751162c
Ayush Senapati <ayush.senapati@cozentus.com>
2023-09-13 11:56:52 +0530

    refactor: Code changes

--- tryproject/tryproject/views.py
+++ tryproject/tryproject/views.py
@@ -16,8 +16,10 @@ def home_view(request, *args, **kwargs):
     
     # from the database??
     article_obj = Article.objects.get(id=random_id)
+    article_queryset = Article.objects.all()
     
     context = {
+        "object_list": article_queryset,
         "object": article_obj,
         "title": article_obj.title,
         "id": article_obj.id,
