15722554505a894e26ffb78600550038fca17337
Ayush Senapati <ayush.senapati@cozentus.com>
2023-09-18 16:26:39 +0530

    fix: article routing id fix

--- tryproject/tryproject/urls.py
+++ tryproject/tryproject/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from articles import views
 from .views import home_view
 
 urlpatterns = [
     path('', home_view),
     path('admin/', admin.site.urls),
+    path('articles/<int:id>/', views.article_detail_view),
 ]
